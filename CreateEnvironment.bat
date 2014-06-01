7d83886748c16f07936d3a2a9d31863db383c5ac
unknown <Administrator@STLR-16.STELLAR.NET>
2014-05-31 23:25:58 -0400

    t

--- CreateEnvironment.bat
+++ CreateEnvironment.bat
@@ -2,11 +2,11 @@ REM This windows Batch File will create the host environment for Mortgage Underw
 REM This Batch file is meant for Windows 7.0 and above
 REM The User who is running the Batch file must be the local Administrator of the machine 
 REM A website named "Default Web Site" must be existing in local IIS.
+IF EXIST "C:\inetpub\wwwroot\MUS" GOTO SKIPENV 
 REM Now Creating the Application Pool ....... 
 C:\Windows\System32\inetsrv\appcmd.exe add apppool /name:"MUSPool"
-IF EXIST "C:\inetpub\wwwroot\MUS" GOTO SKIPDIRETORY 
 REM Creating a physical directory for application............
 mkdir "C:\inetpub\wwwroot\MUS"
- :SKIPDIRETORY
 REM Creating an IIS application under "Default Web Site" whose physical path will be "C:\inetpub\wwwroot\MUS".........
 C:\Windows\System32\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:/MUS /physicalPath:"C:\inetpub\wwwroot\MUS"
+ :SKIPENV
