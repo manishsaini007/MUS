be50de6bb8c998a3d644a79daa6d8d49086b0d02
unknown <Administrator@STLR-16.STELLAR.NET>
2014-06-21 21:08:21 -0400

    rest of the test written

--- MUSTests/BusinessLayer/MortgageCalculatorTests.cs
+++ MUSTests/BusinessLayer/MortgageCalculatorTests.cs
@@ -20,7 +20,7 @@ namespace MUSTests.BusinessLayer
         }
 
         [TestCase()]
-        public void FirstMtgPnITest1()
+        public void FirstMtgPnITest()
         {
 
             _mtgCalculator.FirstLoanAmount = 50000;
@@ -33,7 +33,7 @@ namespace MUSTests.BusinessLayer
         }
 
         [TestCase()]
-        public void SecondMtgPnITest1()
+        public void SecondMtgPnITest()
         {
 
             _mtgCalculator.SecondLoanAmount = 10000;
@@ -43,6 +43,92 @@ namespace MUSTests.BusinessLayer
             double expectedPnI = Math.Round(Financial.Pmt((_mtgCalculator.SecondInterest / 100) / 12, _mtgCalculator.SecondTerms, 0 - _mtgCalculator.SecondLoanAmount, 0, 0), 2);
 
             Assert.AreEqual((Double)expectedPnI, (Double)Math.Round(_mtgCalculator.CalculateSecondPnI(), 2), 0.10);
+        }
+
+        [TestCase()]
+        public void ProposedHousingExpenseTest()
+        {
+
+            
+            _mtgCalculator.FirstLoanAmount = 50000;
+            _mtgCalculator.FirstTerms = 360;
+            _mtgCalculator.FirstInterest = 4.35;
+            _mtgCalculator.SecondLoanAmount = 10000;
+            _mtgCalculator.SecondTerms = 360;
+            _mtgCalculator.SecondInterest = 3.50;
+            _mtgCalculator.ProposedTaxesHOAInsurance = 500;
+            _mtgCalculator.OtherMonthlyDebt = 400;
+            _mtgCalculator.MonthlyIncome = 5000;
+
+            double PnIFirst = Math.Round(Financial.Pmt((_mtgCalculator.FirstInterest / 100) / 12, _mtgCalculator.FirstTerms, 0 - _mtgCalculator.FirstLoanAmount, 0, 0), 2);
+            double PnISecond = Math.Round(Financial.Pmt((_mtgCalculator.SecondInterest / 100) / 12, _mtgCalculator.SecondTerms, 0 - _mtgCalculator.SecondLoanAmount, 0, 0), 2);
+            double expectedHousingExpense = PnIFirst + PnISecond + _mtgCalculator.ProposedTaxesHOAInsurance; 
+
+
+            Assert.AreEqual((Double)expectedHousingExpense, (Double)Math.Round(_mtgCalculator.CalculateProposedHousingExpense(), 2), 0.10);
+        }
+
+        [TestCase()]
+        public void ProposedTotalHousingExpenseTest()
+        {
+
+            _mtgCalculator.FirstLoanAmount = 50000;
+            _mtgCalculator.FirstTerms = 360;
+            _mtgCalculator.FirstInterest = 4.35;
+            _mtgCalculator.SecondLoanAmount = 10000;
+            _mtgCalculator.SecondTerms = 360;
+            _mtgCalculator.SecondInterest = 3.50;
+            _mtgCalculator.ProposedTaxesHOAInsurance = 500;
+            _mtgCalculator.OtherMonthlyDebt = 400;
+            _mtgCalculator.MonthlyIncome = 5000;
+
+            double PnIFirst = Math.Round(Financial.Pmt((_mtgCalculator.FirstInterest / 100) / 12, _mtgCalculator.FirstTerms, 0 - _mtgCalculator.FirstLoanAmount, 0, 0), 2);
+            double PnISecond = Math.Round(Financial.Pmt((_mtgCalculator.SecondInterest / 100) / 12, _mtgCalculator.SecondTerms, 0 - _mtgCalculator.SecondLoanAmount, 0, 0), 2);
+            double expectedTotalHousingExpense = PnIFirst + PnISecond + _mtgCalculator.ProposedTaxesHOAInsurance + _mtgCalculator.OtherMonthlyDebt;
+
+            Assert.AreEqual((Double)expectedTotalHousingExpense, (Double)Math.Round(_mtgCalculator.CalculateTotalHousingExpense(), 2), 0.10);
+        }
+
+        [TestCase()]
+        public void ProposedDTITest()
+        {
+            _mtgCalculator.FirstLoanAmount = 50000;
+            _mtgCalculator.FirstTerms = 360;
+            _mtgCalculator.FirstInterest = 4.35;
+            _mtgCalculator.SecondLoanAmount = 10000;
+            _mtgCalculator.SecondTerms = 360;
+            _mtgCalculator.SecondInterest = 3.50;
+            _mtgCalculator.ProposedTaxesHOAInsurance = 500;
+            _mtgCalculator.OtherMonthlyDebt = 400;
+            _mtgCalculator.MonthlyIncome = 5000;
+
+            double PnIFirst = Math.Round(Financial.Pmt((_mtgCalculator.FirstInterest / 100) / 12, _mtgCalculator.FirstTerms, 0 - _mtgCalculator.FirstLoanAmount, 0, 0), 2);
+            double PnISecond = Math.Round(Financial.Pmt((_mtgCalculator.SecondInterest / 100) / 12, _mtgCalculator.SecondTerms, 0 - _mtgCalculator.SecondLoanAmount, 0, 0), 2);
+            double HousingExpense = PnIFirst + PnISecond + _mtgCalculator.ProposedTaxesHOAInsurance;
+            double expectedDTI = Math.Round((HousingExpense / _mtgCalculator.MonthlyIncome) * 100, 2);
+
+            Assert.AreEqual((Double)expectedDTI, (Double)Math.Round(_mtgCalculator.CalculateDTIRatio(), 2), 0.10);
+        }
+
+        [TestCase()]
+        public void ProposedTDTITest()
+        {
+            _mtgCalculator.FirstLoanAmount = 50000;
+            _mtgCalculator.FirstTerms = 360;
+            _mtgCalculator.FirstInterest = 4.35;
+            _mtgCalculator.SecondLoanAmount = 10000;
+            _mtgCalculator.SecondTerms = 360;
+            _mtgCalculator.SecondInterest = 3.50;
+            _mtgCalculator.ProposedTaxesHOAInsurance = 500;
+            _mtgCalculator.OtherMonthlyDebt = 400;
+            _mtgCalculator.MonthlyIncome = 5000;
+
+            double PnIFirst = Math.Round(Financial.Pmt((_mtgCalculator.FirstInterest / 100) / 12, _mtgCalculator.FirstTerms, 0 - _mtgCalculator.FirstLoanAmount, 0, 0), 2);
+            double PnISecond = Math.Round(Financial.Pmt((_mtgCalculator.SecondInterest / 100) / 12, _mtgCalculator.SecondTerms, 0 - _mtgCalculator.SecondLoanAmount, 0, 0), 2);
+            double totalHousingExpense = PnIFirst + PnISecond + _mtgCalculator.ProposedTaxesHOAInsurance + _mtgCalculator.OtherMonthlyDebt;
+            double expectedTDTI = Math.Round((totalHousingExpense / _mtgCalculator.MonthlyIncome) * 100, 2);
+
+            Assert.AreEqual((Double)expectedTDTI, (Double)Math.Round(_mtgCalculator.CalculateTDTIRatio(), 2), 0.10);
         }
 
         [TestFixtureTearDown()]
